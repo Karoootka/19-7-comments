9903304e6468d5b51a18ea69702937d63e80bd5e
Karolina Madej <kkarolinamadej@gmail.com>
2018-07-20 16:38:36 +0200

    Add thumb up and down

--- src/comments.js
+++ src/comments.js
@@ -12,20 +12,17 @@ function comments(state = [], action) {
       ...state];
 
     case REMOVE_COMMENT:
-      return
-        state.filter(comment => comment.id !== action.id);
+      return state.filter(comment => comment.id !== action.id);
 
     case EDIT_COMMENT:
-      return
-        state.map(comment => {
+      return state.map(comment => {
           if (comment.id === action.id) {
             comment.text = action.text
           }
         });
 
     case THUMB_UP_COMMENT:
-      return
-        state.map(comment => {
+      return state.map(comment => {
           if (comment.id === action.id) {
             return {...comment, votes: comment.votes + 1}
           }
@@ -33,8 +30,7 @@ function comments(state = [], action) {
         });
 
     case THUMB_DOWN_COMMENT:
-      return
-        state.map(comment => {
+      return state.map(comment => {
           if (comment.id === action.id) {
             return {...comment, votes: comment.votes - 1}
           }
